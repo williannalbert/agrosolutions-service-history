de12558b663ffa261d9436b2c11b9cdc7835f953
Willian Nalbert <williannalbert@hotmail.com>
2026-02-01 11:49:45 -0300

    inclusão de classe de configuração da Program.cs

--- AgroSolutions.History.API/Program.cs
+++ AgroSolutions.History.API/Program.cs
@@ -1,3 +1,4 @@
+using AgroSolutions.History.API.Configuration;
 using AgroSolutions.History.API.Middlewares;
 using AgroSolutions.History.Application.Extensions;  
 using AgroSolutions.History.Infrastructure.Extensions;
@@ -7,58 +8,10 @@ using Serilog.Sinks.Elasticsearch;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAll", builder =>
-    {
-        builder.AllowAnyOrigin() 
-               .AllowAnyMethod()  
-               .AllowAnyHeader(); 
-    });
-});
-
-builder.Services.AddInfrastructureServices(builder.Configuration);
-builder.Services.AddApplicationServices();
-
-builder.Host.UseSerilog((context, configuration) =>
-{
-    var elasticUri = context.Configuration["ElasticConfiguration:Uri"] ?? "http://localhost:9200";
-
-    configuration
-        .Enrich.FromLogContext()
-        .Enrich.WithMachineName()
-        .WriteTo.Console() 
-        .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(elasticUri))
-        {
-            IndexFormat = $"agro-history-logs-{DateTime.UtcNow:yyyy.MM.dd}",
-            AutoRegisterTemplate = true, 
-            NumberOfShards = 2,
-            NumberOfReplicas = 1
-        })
-        .ReadFrom.Configuration(context.Configuration); 
-});
+builder.AddApiConfiguration();
 
 var app = builder.Build();
 
-app.UseMiddleware<ErrorHandlingMiddleware>();
-
-app.UseSerilogRequestLogging();
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-app.UseCors("AllowAll");
-
-app.UseAuthorization();
-
-app.MapControllers();
+app.UseApiConfiguration();
 
 app.Run();
